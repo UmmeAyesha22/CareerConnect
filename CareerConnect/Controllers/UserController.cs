1c09de4b5fa7cabd67e629fa4b3db7ae5ca344de
UmmeAyesha22 <ummeayesha168@gmail.com>
2025-09-12 02:02:14 +0600

    Registration UI

---
+++ CareerConnect/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using CareerConnect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CareerConnect.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult NewUser()
+        {
+            return View(new UserMV());
+        }
+    }
+}
